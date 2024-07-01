3dcea7d6eb690350a411a49e76e55c3a2b0b2fcb
NhatPham <phamvuonghoangnhat@gmail.com>
2024-07-01 22:32:50 +0700

    update ui

--- AiChatBot/App.tsx
+++ AiChatBot/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>AiChatBot</Text>
+      <Text>AiChatBot after 1 years</Text>
       <StatusBar style="auto" />
     </View>
   );

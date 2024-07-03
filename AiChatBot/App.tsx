f306d6069906d657762f33eed14550286f76d560
NhatPham <phamvuonghoangnhat@gmail.com>
2024-07-03 22:51:09 +0700

    update prompt

--- AiChatBot/App.tsx
+++ AiChatBot/App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [query, setQuery] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>AiChatBot after 1 years</Text>
+      <TextInput
+        placeholder="prompt"
+        value={query}
+        onChangeText={setQuery}
+        style={{
+          padding: 10,
+          borderColor: "gray",
+          borderWidth: 1,
+          width: 200,
+          margin: 10,
+          borderRadius: 5,
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+
     justifyContent: "center",
   },
 });
